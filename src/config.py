b19d5ed23e43242fa171ebaf0de64a9343dbea69
Wesley Freeman-Smith <wfreemansmith.dev@gmail.com>
2025-03-27 09:01:09 +0000

    added DAO functionality

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 
 ENV = os.getenv("APP_ENV", "development")
@@ -11,5 +12,18 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_PORT = os.getenv("DB_PORT")
+LEGACY_DB_HOST = os.getenv("LEGACY_DB_HOST")
+LEGACY_DB_NAME = os.getenv("LEGACY_DB_NAME")
+USERDOMAIN = os.getenv("USERDOMAIN")
+USERNAME = os.getenv("USERNAME")
+
+# debug mode
+parser = argparse.ArgumentParser()
+parser.add_argument('-v', '--verbose', action='store_true', dest='debug')
+args = parser.parse_args()
+
+DEBUG = args.debug
 
 print(f"Running in {ENV} environment")
+if DEBUG:
+    print("Debug mode on")
--- src/db.py
+++ src/db.py
@@ -1,10 +1,24 @@
-from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME
+from config import DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, LEGACY_DB_HOST, LEGACY_DB_NAME, USERDOMAIN, USERNAME
 import psycopg2
+import pyodbc
 
 
 def get_database_connection():
     """Returns connection object to database"""
     DB_STRING = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     connection = psycopg2.connect(DB_STRING)
+
+    return connection
+
+
+def get_legacy_database_connection():
+    """Returns connection object to legacy SQL Server database"""
+    connection = pyodbc.connect(
+        driver = "{ODBC Driver 11 for SQL Server}",
+        host = LEGACY_DB_HOST,
+        database = LEGACY_DB_NAME,
+        user = f"{USERDOMAIN}\\{USERNAME}",
+        Trusted_Connection="yes"
+    )
 
     return connection
