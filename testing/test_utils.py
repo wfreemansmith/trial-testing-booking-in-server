7ff9bf75ff07e2a932e2a14cabd8e4b7eceabcb3
Wesley Freeman-Smith <wfreemansmith.dev@gmail.com>
2025-05-16 07:45:38 +0100

    helpful functions added

--- testing/test_utils.py
+++ testing/test_utils.py
@@ -1,8 +1,9 @@
-from src.utils import format_version_id
+from src.utils import format_version_id, get_candidate_range, url_contructor, message_editor
 import pytest
 
+
 @pytest.mark.parametrize(
-        "paper, component, version, result", 
+        "paper, component, version, expected", 
         [
             ("AC", "R", "IP123", "ACRIP123"),
             ("AC", "Reading", "IP123", "ACRIP123"),
@@ -12,6 +13,45 @@ import pytest
             ("AC", "Listening", "BP43", "LBP43")
         ]
     )
-def test_format_version_id(paper, component, version, result):
+def test_format_version_id(paper, component, version, expected):
     output = format_version_id(paper=paper, component=component, version=version)
-    assert output == result
+    assert output == expected
+
+
+@pytest.mark.parametrize(
+        "args, expected",
+        [
+            ( ["AOG/IELTS Ops/", "Trial Testing/"], "AOG/IELTS Ops/Trial Testing/" ),
+            ( ["AOG", "IELTS Ops", "Trial Testing"], "AOG/IELTS Ops/Trial Testing/" )
+        ]
+    )
+def test_url_helper(args, expected):
+    result = url_contructor(*args)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+        "candidate_nums, expected",
+        [
+            ([1, 2, 3, 4, 5], "0001-0005"),
+            ([34, 12, 99, 102], "0012-0102"),
+            ([89], "0089"),
+            (["34", 32], "0032-0034"),
+            ([], None)
+        ]
+)
+def test_get_candidate_range(candidate_nums, expected):
+    output = get_candidate_range(candidate_nums)
+    assert output == expected
+
+
+@pytest.mark.parametrize(
+        "message, kwargs, expected",
+        [
+            ( "My name is {name}", {"name": "David"}, "My name is David" ),
+            ( "David is a {blank} {noun}", {"blank": "Great", "noun": "Duck"}, "David is a Great Duck" )
+        ]
+    )
+def test_message_editor(message, kwargs, expected):
+    result = message_editor(message, **kwargs)
+    assert result == expected
