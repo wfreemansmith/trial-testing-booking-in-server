1449c64c0fec5adafc8f763941b12dd795c892ff
Wesley Freeman-Smith <freemansmith.w@cambridgeenglish.org>
2025-05-23 14:42:03 +0100

    docker apparently working

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.12-slim
-WORKDIR /src
-COPY requirements.txt /src/
+WORKDIR /app
+COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+RUN apt-get update && apt-get install -y unixodbc unixodbc-dev
 COPY . .
-CMD ["python", "-m", "src.main.py"]
+CMD ["python", "-m", "src.main"]
--- makedev.bat
+++ makedev.bat
@@ -2,25 +2,30 @@
 if "%1"=="build" goto build
 if "%1"=="up" goto up
 if "%1"=="down" goto down
+if "%1"=="reset" goto reset
 if "%1"=="test" goto test
 
 echo Unknown command: %1
 goto end
 
 :build
-docker-compose --env-file .env.development up --build
+docker-compose up --build
 goto end
 
 :up
-docker-compose --env-file .env.development up
+docker-compose up
 goto end
 
 :down
 docker-compose down
 goto end
 
+:reset
+docker-compose down -v
+goto end
+
 :test
-docker-compose run --rm tests
+docker compose run --rm tests
 docker-compose down
 goto end
 
